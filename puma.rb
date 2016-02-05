3ae869b5d0a3dfb60343a488fb5d3e20db157090
chen <kikyous@163.com>
2016-02-05 20:58:58 +0800

    Update puma.rb

--- puma.rb
+++ puma.rb
@@ -1,4 +1,15 @@
 workers 2
 threads 1, 6
+preload_app!
 environment 'production'
 bind "unix:///tmp/puma.sock"
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
